4353ccf6eb6ea18719da0bf9b3452294e5f87422
RiZZeR99 <ramusteata@gmail.com>
2023-01-02 22:09:56 +0200

    Integrated model in the new back-end. Changed URL address.

--- work/lab5/Flusk_backend/backend.py
+++ work/lab5/Flusk_backend/backend.py
@@ -1,4 +1,19 @@
 from flask import Flask, request
+import base64
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from PIL import Image, ImageFile
+import sys
+from io import BytesIO
+
+from model import AE
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+device = 'cpu'
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,10 +22,56 @@ app.config["DEBUG"] = True
 def predictAPI():
     content = request.get_json()
     print(content)
-    return predictImage(content['image'],content['years'])
+    return predictImage(content['image'],content['yearsInFuture'])
 
-def predictImage(image, years):
-    # todo add here implementation for the model
-    return image
+def predictImage(imageString, yearsInFuture):
+    device = 'cpu'
+    model = AE().to(device)
+    model.load_state_dict(torch.load(
+        r'model_weightsM_20.pth',
+        map_location=torch.device('cpu')))
+    model.eval()
+
+    image = Image.open(BytesIO(base64.b64decode(imageString)))
+    image = image.resize((576, 576))
+
+    patch_size = 32
+    width_index = image.width // patch_size
+    height_index = image.height // patch_size
+    new_image = Image.new(mode="RGB", size=image.size)
+    
+    for index_w in range(width_index + 1):
+        for index_h in range(height_index + 1):
+            cut_point_x = index_w * patch_size
+            cut_point_y = index_h * patch_size
+            image_crop = image.crop((cut_point_x,
+                                    cut_point_y,
+                                    cut_point_x + patch_size,
+                                    cut_point_y + patch_size))
+            image_crop = np.array(Image.fromarray(np.array(image_crop), 'RGB'))
+
+            image_crop = torch.Tensor(image_crop.reshape(1, 3, 32, 32))
+            transform = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            image_crop = transform(image_crop)
+
+            time_skip = torch.Tensor([yearsInFuture]).to(device)
+            image_crop = image_crop.to(device)
+
+            with torch.no_grad():
+                pred = model(image_crop, time_skip)
+
+            pred = pred.detach().cpu().numpy().reshape((32, 32, 3, 1)).squeeze()
+            img = Image.fromarray(pred, 'RGB')
+            new_image.paste(img, (cut_point_x, cut_point_y))
+
+    buffered = BytesIO()
+    new_image.save(buffered, format="JPEG")
+    image_bytes = base64.b64encode(buffered.getvalue())
+    image_string_result = image_bytes.decode('ascii')
+
+    return {
+        "image":image_string_result,
+        "predictedPrice":-1.0
+    }
 
 app.run()
---
+++ work/lab5/Flusk_backend/model.py
@@ -0,0 +1,129 @@
+import torch
+import torch.functional as F
+import torchvision.transforms as transforms
+from torch import nn
+import numpy as np
+import os
+
+class Encoder(nn.Module):
+    def __init__(self, encoded_space_dim, fc2_input_dim):
+        super().__init__()
+        
+        self.encoder_cnn = nn.Sequential(
+            nn.Conv2d(3, 8, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            nn.ReLU(True)
+        )
+        self.layers_cnn = []
+        self.layers_cnn.append(nn.Conv2d(3, 8, 3, stride=2, padding=1))
+        self.layers_cnn.append(nn.ReLU(True))
+        self.layers_cnn.append(nn.Conv2d(8, 16, 3, stride=2, padding=1))
+        self.layers_cnn.append(nn.BatchNorm2d(16))
+        self.layers_cnn.append(nn.ReLU(True))
+        self.layers_cnn.append(nn.Conv2d(16, 32, 3, stride=2, padding=0))
+        self.layers_cnn.append(nn.ReLU(True))
+        
+        self.flatten = nn.Flatten(start_dim=1)
+        
+        self.layers_lin = []
+        self.layers_lin.append(nn.Linear(3 * 3 * 32, 128))
+        self.layers_lin.append(nn.ReLU(True))
+        self.layers_lin.append(nn.Linear(128, encoded_space_dim))
+        
+        self.encoder_lin = nn.Sequential(
+            nn.Linear(3 * 3 * 32, 128),
+            nn.ReLU(True),
+            nn.Linear(128, encoded_space_dim)
+        )
+        
+    def forward(self, x):
+#         print("------------------------------------\nEncoding\n------------------------------------\n")
+#         print(f"Initial shape: {x.shape}")
+#         for i in range(0, len(self.layers_cnn)):
+#             x = self.layers_cnn[i](x)
+#             #print(f"{i} : {x.shape}")
+        x = self.encoder_cnn(x)
+        x = self.flatten(x)
+        #print(f"After flatten: {x.shape}")
+        x = self.encoder_lin(x)
+#         for i in range(0, len(self.layers_lin)):
+#             x = self.layers_lin[i](x)
+#             #print(f"{i} : {x.shape}")
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, encoded_space_dim,fc2_input_dim):
+        super().__init__()
+        self.layers_cnn = []
+        self.layers_cnn.append(nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=1))
+        self.layers_cnn.append(nn.BatchNorm2d(16))
+        self.layers_cnn.append(nn.ReLU(True))
+        self.layers_cnn.append(nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1))
+        self.layers_cnn.append(nn.BatchNorm2d(8))
+        self.layers_cnn.append(nn.ReLU(True))
+        self.layers_cnn.append(nn.ConvTranspose2d(8, 3, 3, stride=2, padding=1, output_padding=1))
+
+        
+        self.layers_lin = []
+        self.layers_lin.append(nn.Linear(encoded_space_dim, 128))
+        self.layers_lin.append(nn.ReLU(True))
+        self.layers_lin.append(nn.Linear(128, 3 * 3 * 32))
+        self.layers_lin.append(nn.ReLU(True))
+        
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(encoded_space_dim, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 3, 3, stride=2, padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+#         print("\n------------------------------------\nDecoding\n------------------------------------\n")
+#         print(f"Initial shape: {x.shape}")
+#         for i in range(0, len(self.layers_lin)):
+#             x = self.layers_lin[i](x)
+#             #print(f"{i} : {x.shape}")
+        #print(f"Shape after unflatten: {x.shape}")
+        
+#         for i in range(0, len(self.layers_cnn)):
+#             x = self.layers_cnn[i](x)
+#             #print(f"{i} : {x.shape}")
+        
+        
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        self.temporal_embedding = nn.Parameter(torch.rand(1, 3, 32, 32))
+        self.encoder = Encoder(32*32*3, 128)
+        self.decoder = Decoder(32*32*3, 128)
+
+    def forward(self, x, time_skip):
+        time_skip_vector = torch.einsum('i, iabc->iabc', time_skip, self.temporal_embedding.repeat(time_skip.size(0), 1, 1, 1))
+        x = x + time_skip_vector
+        x = self.encoder(x)
+        # x = x + time_skip_vector
+        x = self.decoder(x)
+        return x
